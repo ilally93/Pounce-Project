058d7621380dfa58dcd39d03257070820fe936de
Chloe Harris <harriscn@g.cofc.edu>
2020-09-19 23:53:12 -0400

    Final Push for Static Demo(?)

--- lib/pets.dart
+++ lib/pets.dart
@@ -18,22 +18,26 @@ class Pet {
   }
 }
 
-List<Pet> pets = [
+List<Pet> petList = [
   Pet(name: 'Dory', age: '1', species: 'Cat', picture: "assets/pets/Dory.png"),
-  Pet(name: 'Carly', age: '1', species: 'Cat', picture: "assets/pets/Carly.png"),
+  Pet(name: 'Carly', age: '6', species: 'Cat', picture: "assets/pets/Carly.png"),
   Pet(name: 'Carl', age: '7', species: 'Corgi mix', picture: "assets/pets/Carl.png"),
   Pet(name: 'Siri', age: '1', species: 'Cat', picture: "assets/pets/Siri.png"),
   Pet(name: 'Earl', age: '1', species: 'Cat', picture: "assets/pets/Earl.png"),
-  Pet(name: 'Inky', age: '1', species: 'Cat', picture: "assets/pets/Inky.png"),
-  Pet(name: 'Pepper', age: '1', species: 'Cat', picture: "assets/pets/Pepper.png"),
+  Pet(name: 'Inky', age: '6', species: 'Cat', picture: "assets/pets/Inky.png"),
+  Pet(name: 'Pepper', age: '8', species: 'Cat', picture: "assets/pets/Pepper.png"),
   Pet(name: 'Tuki', age: '12', species: 'Green Cheek Conure', picture: "assets/pets/Tuki.png"),
   Pet(name: 'Pebbles', age: '10', species: 'Yorkie', picture: "assets/pets/Pebbles.png"),
   Pet(name: 'Honey', age: '12', species: 'Yorkie', picture: "assets/pets/Honey.png"),
   Pet(name: 'Bear', age: '5', species: 'Cat', picture: "assets/pets/Bear.png"),
-  Pet(name: 'Rico', age: '6', species: 'Pit mix', picture: "assets/pets/Rico.png"),
+  Pet(name: 'Rico', age: '7', species: 'Pit mix', picture: "assets/pets/Rico.png"),
   Pet(name: 'Cloudy', age: '10', species: 'Cat', picture: "assets/pets/Cloudy.png"),
-  Pet(name: 'Fluff', age: '1', species: 'Cat', picture: "assets/pets/Fluff.png"),
+  Pet(name: 'Fluff', age: '9', species: 'Cat', picture: "assets/pets/Fluff.png"),
 ];
+
+String getPet(pet) {
+  return pet;
+}
 
 List<String> petNames = [
   'Bear',
--- lib/pounce_page.dart
+++ lib/pounce_page.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
+import 'pets.dart';
 
-class Pet {
-  String name;
-  String age;
-  String species;
-  String breed;
-  bool hearted;
-
-  Pet({ this.name, this.age, this.species, this.breed, this.hearted });
-}
 
 class Pounce extends StatefulWidget {
   @override
@@ -17,20 +9,13 @@ class Pounce extends StatefulWidget {
 
 class _PounceState extends State<Pounce> {
 
-  List<Pet> pets = [
-    Pet(name: 'Dory', age: '1', species: 'Cat', breed: '', hearted: false),
-    Pet(name: 'Earl', age: '1', species: 'Cat', breed: '', hearted: false),
-    Pet(name: 'Siri', age: '1', species: 'Cat', breed: '', hearted: false),
-    Pet(name: 'Carl', age: '1', species: 'Dog', breed: 'Corgi mix', hearted: false),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[200],
       body: Container(
-            child: PetCard(pet: pets[1]),
-      )
+        child: PetCard(pet: petList[4]),
+      ),
     );
   }
 }
@@ -44,7 +29,7 @@ class PetCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         color: Colors.grey[300],
-        margin: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 0.0),
+        margin: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 20.0),
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Column(
