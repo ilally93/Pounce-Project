2c6d9fa747502b1eb1ba8ab23223c348143fdfa1
Chloe Harris <harriscn@g.cofc.edu>
2020-09-19 21:34:24 -0400

    Updated Pounce Page and Dependencies

--- lib/pounce_page.dart
+++ lib/pounce_page.dart
@@ -19,20 +19,21 @@ class _PounceState extends State<Pounce> {
 
   List<Pet> pets = [
     Pet(name: 'Dory', age: '1', species: 'Cat', breed: '', hearted: false),
-    //Pet(name: 'Earl', age: 1, species: 'Cat', breed: '', hearted: false),
-    //Pet(name: 'Siri', age: 1, species: 'Cat', breed: '', hearted: false),
-    //Pet(name: 'Carl', age: 1, species: 'Dog', breed: 'Corgi mix', hearted: false),
+    Pet(name: 'Earl', age: '1', species: 'Cat', breed: '', hearted: false),
+    //Pet(name: 'Siri', age: '1', species: 'Cat', breed: '', hearted: false),
+    //Pet(name: 'Carl', age: '1', species: 'Dog', breed: 'Corgi mix', hearted: false),
   ];
 
   List<String> petNames = [
     'Dory',
-    //'Earl',
+    'Earl',
     //'Siri',
     //'Carl',
   ];
 
   Widget petCard(pet) {
     return Card(
+        color: Colors.grey[300],
         margin: EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 0.0),
         child: Padding(
           padding: const EdgeInsets.all(12.0),
@@ -41,21 +42,35 @@ class _PounceState extends State<Pounce> {
               Text(
                 pet.name,
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
                 )
               ),
               Image(
-                image: AssetImage('assets/pets/Dory.jpg'),
-                height: 75,
-                width: 75,
+                image: AssetImage('assets/pets/${pet.name}.png'),
+                height: 400,
+                width: 400,
               ),
               Row(
                 children: <Widget>[
                   Expanded(
-                    child: Text(pet.species)
+                    child: Text(
+                      pet.species,
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold
+                      )
+                    )
                   ),
                   Expanded(
-                    child: Text(pet.age, textAlign: TextAlign.right)
+                    child: Text(
+                      'Age: ' + pet.age,
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold
+                      )
+                    )
                   )
                 ]
               )
@@ -69,8 +84,14 @@ class _PounceState extends State<Pounce> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[200],
-      body: Column(
-        children: pets.map((pet) => petCard(pet)).toList(),
+      body: Container(
+        child: Column(
+          children: <Widget> [
+            petCard('Dory')
+          ]
+        ),
+
+
       )
     );
   }
